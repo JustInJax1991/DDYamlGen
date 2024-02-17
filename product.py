3131a8d9fab9eb0e105b4b2ade6b117c3563a09e
Justin Odel <justinodel70@gmail.com>
2024-02-17 15:58:20 -0500

    Update to write out a string to file instead of yaml dump due to formatting issues

--- product.py
+++ product.py
@@ -1,21 +1,8 @@
 import yaml
 import pandas as pd
 import socket
-
-'''
-NOTES
-Parameters Required:
-sheet name
-tid
-truck
-
-To-DO
-We need to create the other system lists(point the script to these)
-We need to include the scripts parameters
-We need to figure out the download portion of the CamAssign.xlsx file
-'''
-
-
+import sys
+import re
 
 
 """
@@ -26,47 +13,30 @@ excel = pd.read_excel(r".\CamAssign.xlsx", sheet_name="AT&T Pebble Beach", useco
 
 data_top = excel.columns
 data_missing = excel.isna()
-yamltemplate = ''''''
-tournamentID = 'r2024005'
-truck = 'sl1'
 
 i = 0
-
+devices= ""
 while(i < 18):
     k = 0
     for x in excel.iloc[i]:
         
         if(not(data_missing.iloc[i][k])):
             
-            print("###################################")
-            print("Hole: " + str(i + 1))
-            print("Camera: " + str(x))
-            print("Location: " + str(data_top[k]))
             cam = str(x)
             cameraName = cam[0:4] + 'B' +  cam[4:] 
-            holeLocation = str(i+1)
+            holeLocation = i + 1
             cameraLocation = str(data_top[k]).lower()
             if cameraLocation == 'tee':
                 cameraLocation += '1'
             cameraIp = socket.gethostbyname(cameraName)
-            yamltemplate += '''- host: {0}
-  timeout: 1.0
-  tags:
-  - tournament:{1}
-  - location:{2}
-  - hole:{3}
-  - bolt6tfg:{4}
-  - ip:{0}
-  - rack:case
-  - nightly:False
-  - hardware:camera
-  - type:physical
-  - env:prod
-  - name:{5}
-  - case:none\n'''.format(cameraIp,tournamentID,truck,holeLocation,cameraLocation,(cameraName.lower()))
+            toWrite = "\n- host: {0}\n  timeout: 1.0\n  tags:\n  - tournament:{1}\n  - location:{2}\n  - hole:{3}\n  - bolt6tfg:{4}\n  - ip:{0}\n  - rack:case\n  - nightly:False\n  - hardware:camera\n  - type:physical\n  - env:prod\n  - name:{5}\n  - case:none".format(cameraIp, tournamentID.lower(), truck.lower(), holeLocation, cameraLocation, cameraName.lower())
+            devices = devices + toWrite
         k = k + 1
     i = i + 1
 
+'''
+Lets build the PI device portion of the YAML
+'''
 power_excel = pd.read_excel(r".\CamAssign.xlsx", sheet_name="AT&T Pebble Beach", usecols='B:K', nrows=18, header=22) 
 
 data_top = power_excel.columns
@@ -77,49 +47,29 @@ i = 0
 while(i < 18):
     k = 0
     for x in power_excel.iloc[i]:
+        caseNum = re.search("\d?\d\d\d",str(x))
         
-        if((not(data_missing.iloc[i][k])) and ("SC" in x)):
-            
-            print("###################################")
-            print("Hole: " + str(i + 1))
-            print("Power: " + str(x))
-            print("Location: " + str(data_top[k]))
-            casetemp = str(x)
-            caseName = 'PI-SC0' + casetemp[3:]
-            with open (r".\PiIPSysListSL1.txt",'r') as pilist:
+        if(not(data_missing.iloc[i][k]) and (caseNum)):
+            caseName = 'PI-SC0' + str(caseNum.group())
+            with open(f"PiIPSysList{truck}.txt", 'r') as pilist:
                 lines = pilist.readlines()
                 for line in lines:
                     if line.find(caseName) != -1:
                         caseLine = lines.index(line)
+                piLocation = str(data_top[k]).lower()
+                if 'tee' in piLocation:
+                        piLocation += '1'
             caseIp = (lines[caseLine])[10:-1]
             picase = caseName.lower()
-            hole = str(i+1)
-            holeLocation = str(data_top[k]).lower()
-            if holeLocation == 'tee':
-                holeLocation += '1'
-            yamltemplate+='''- host: {0}
-            timeout: 1.0
-            tags:
-            - ip:{0}
-            - env:prod
-            - type:physical
-            - rack:case
-            - hardware:pi
-            - network:True
-            - case:{1}
-            - name:{1}
-            - os:none
-            - priority:none
-            - grn_server:False
-            - location:{2}
-            - tournament:{3}
-            - bolt6tfg:{4}
-            - hole:{5}\n'''.format(caseIp,picase,truck,tournamentID,holeLocation,hole)
+            hole = i + 1
+            toWrite = "- host: {0}\n  timeout: 1.0\n  tags:\n  - ip:{0}\n  - env:prod\n  - type:physical\n  - rack:case\n  - hardware:pi\n  - network:True\n  - case:{1}\n  - name:{1}\n  - os:none\n  - priority:none\n  - grn_server:False\n  - location:{2}\n  - tournament:{3}\n  - bolt6tfg:{4}\n  - hole:{5}".format(caseIp, picase, truck.lower(), tournamentID.lower(), piLocation, hole)      
+            devices = devices + toWrite
         k = k + 1
     i = i + 1
 
-f = open('conf.yaml','a')
-f.write(yamltemplate)
+with open(f'slxddp1pingconf.yaml', 'w',) as f :
+        f.write(devices)
+
 f.close()
 
 
